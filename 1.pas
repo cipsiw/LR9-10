64aaa2958e1c22fc1a61adba49e0922607388f7b
cipsiw <118716892+cipsiw@users.noreply.github.com>
2022-12-12 15:15:05 +0300

    Add files via upload

---
+++ 1.pas
@@ -0,0 +1,17 @@
+﻿function shi(s, s1: integer) := s * s1;
+const
+  n = 5;
+var
+  a: array[1..n] of integer;
+  i, s, s1, s2: integer;
+begin
+  for i := 1 to n do
+    read(a[i]);
+  s := 1;
+  for i := 1 to n do
+    if i mod 2 = 0 then begin
+      s1 := a[i];
+      s := shi(s, s1);
+    end;
+  writeln(s);
+end.
